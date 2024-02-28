3e47b5de9c65dd9062560650b5b7b69eb17ea8bc
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-02-28 23:58:42 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from "astro/config"
+
+import solidJs from "@astrojs/solid-js"
+import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [solidJs({}), tailwind({})],
+	output: "server",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true
+		}
+	}),
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "es"],
+		routing: {
+			prefixDefaultLocale: false
+		}
+	}
+})
