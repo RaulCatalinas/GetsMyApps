04ba117a335d10f5a27bd71fc80df67af9e7b9a2
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-03-01 17:19:45 +0100

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,9 +3,6 @@
 interface ImportMetaEnv {
 	readonly SUPABASE_DB_PASSWORD: string
 	readonly SUPABASE_API_KEY: string
-	readonly CLOUDINARY_API_KEY: string
-	readonly CLOUDINARY_API_SECRET: string
-	readonly CLOUDINARY_CLOUD_NAME: string
 }
 
 interface ImportMeta {
