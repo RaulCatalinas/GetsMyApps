aada1a3ccb71751613d07f5128ad9b77355b1d45
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-03-03 19:36:55 +0100

    feat(Root): Add icon to download button
    - Enhanced download button by adding an icon for better visual representation.

--- src/components/buttons/DownloadButton.tsx
+++ src/components/buttons/DownloadButton.tsx
@@ -1,4 +1,8 @@
+// Controllers
 import { downloadController } from "@/controllers/download-controller"
+
+// Icons
+import DownloadIcon from "@/icons/buttons/DownloadIcon"
 
 interface Props {
 	githubRepoName: string
@@ -8,10 +12,11 @@ export default function DownloadButton({ githubRepoName }: Props) {
 	return (
 		<button
 			type="button"
-			class="bg-green-500 hover:bg-green-600 hover:scale-105 active:scale-90 text-black font-bold py-2 px-4 rounded w-full text-[22px]"
+			class="bg-green-500 hover:bg-green-600 hover:scale-105 active:scale-90 text-black font-bold py-2 px-4 rounded w-full flex justify-center items-center"
 			onClick={async () => await downloadController(githubRepoName)}
 		>
-			Download
+			<DownloadIcon />
+			<span class="pl-2 text-2xl text-center">Download</span>
 		</button>
 	)
 }
