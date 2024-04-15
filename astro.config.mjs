95d2c2a8408ee800180dda84bd5814815f984e2a
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-04-15 18:32:53 +0200

    feat(Root): Implement @astrojs/sitemap integration for automatic sitemap generation
    - Utilized @astrojs/sitemap integration to automatically generate the website sitemap.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config'
-
+import sitemap from '@astrojs/sitemap'
 import solidJs from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs({}), tailwind({})],
+  site: 'https://gets-my-apps.vercel.app',
+  integrations: [solidJs(), tailwind(), sitemap()],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
